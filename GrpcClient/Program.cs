8bd972a3ee7012771d8cf02d73f4a33d4ff25cfc
ladni <ladnik@hotmail.com>
2021-03-08 21:06:12 +0200

    Init. commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        const int PORT = 19019;
+
+        static async Task<int> Main(string[] args)
+        {
+            Console.WriteLine("GrpcClient started.");
+
+            var channel = new Channel($"localhost:{PORT}",
+#if TLS
+                new SslCredentials(File.ReadAllText(@"Certs\certificate.crt")));
+#else
+                ChannelCredentials.Insecure
+#endif
+            );
+            var nl = Environment.NewLine;
+            var orgTextColor = Console.ForegroundColor;
+
+            var client = new Client();
+            await client.Do(
+                channel, 
+                () =>
+                {
+                    Console.Write($"Connected to server.{nl}ClientId = ");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write($"{client.ClientId}");
+                    Console.ForegroundColor = orgTextColor;
+                    Console.WriteLine($".{nl}Enter string message to server.{nl}" +
+                        $"You will get response if your message will contain question mark '?'.{nl}" +
+                        $"Enter empty message to quit.{nl}");
+                },
+                () => Console.WriteLine("Shutting down...")
+            );
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
---
+++ GrpcServer/Program.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace GrpcServer
+{
+    public class Program
+    {
+        const int PORT = 19019;
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .ConfigureKestrel(options =>
+                    {
+                        options.Limits.MinRequestBodyDataRate = null;
+                        options.Listen(IPAddress.Any, PORT
+#if TLS
+                        , listenOptions =>
+                        {
+                            listenOptions.UseHttps("grpcServer.pfx", "1511");
+                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        }
+#endif                        
+                        );
+                    });
+                });
+    }
+}
